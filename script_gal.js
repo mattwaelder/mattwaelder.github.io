c796e6b5f9f82ef1ffe329c8464675abf6c5bc9b
mattwaelder <mattwaelder@gmail.com>
2022-05-16 23:33:51 -0700

    <intersectionObserver api lazy loading v1>

--- script_gal.js
+++ script_gal.js
@@ -4,24 +4,14 @@ console.log("hello mattspigs gallery");
 
 const galleryContainer = document.querySelector(".gallery");
 
-//////////////////////////////////////////////////
-/* this is node js, it would be nice to use this but ill hard code things for now until i learn more about using nodejs */
-//////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+/* populating gallery with (lazy) images */
+//////////////////////////////////////////////////////////////////////////////
 
-// const { promises: fs } = require("fs");
-// const dir = "./media/gallery_imgs";
-
-// const getNumFiles = async (dir) => {
-//   const files = await fs.readdir(dir);
-//   console.log("getnumfn");
-//   console.log(files.length);
-// };
-// getNumFiles(dir);
-
-////////////////////////////////////////////////
 const selectedImgContainer = document.getElementById("selected_img_container");
 const selectedImg = document.getElementById("selected_img");
 
+//fn to populate the gallery
 const populateGal = function () {
   let selectedIndex = null;
 
@@ -29,13 +19,14 @@ const populateGal = function () {
   //make new array with length equal to amount of pics in gallery
   const countArr = new Array(46);
 
-  //this would be better done by using datasets, adding a dataset value and pulling that from a click event
-
+  //create new images with data-src;
   for (let i = 1; i < countArr.length + 1; i++) {
     let curImg = document.createElement("img");
-    curImg.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
-    curImg.loading = "lazy";
-    curImg.classList.add("gal_img");
+    // curImg.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
+    curImg.dataset.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
+    // curImg.loading = "lazy";
+    // curImg.classList.add("gal_img");
+    curImg.classList.add("gal_img_lazy");
 
     //event listener for modal functionality
     curImg.addEventListener("click", () => {
@@ -54,3 +45,26 @@ selectedImgContainer.addEventListener("click", function () {
   selectedImgContainer.style.transform = `translateY(-100%)`;
   // selectedImgContainer.src = "";
 });
+
+const lazyImgs = document.querySelectorAll("img[data-src]");
+
+const imgLoader = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  console.log(entry.target);
+
+  entry.target.src = entry.target.dataset.src;
+  entry.target.classList.remove("gal_img_lazy");
+  entry.target.classList.add("gal_img");
+  observer.unobserve(entry.target);
+};
+
+const settingsObj = {
+  root: null,
+  threshold: 0,
+  reootMargin: "200px",
+};
+
+const imgObserver = new IntersectionObserver(imgLoader, settingsObj);
+
+lazyImgs.forEach((img) => imgObserver.observe(img));
