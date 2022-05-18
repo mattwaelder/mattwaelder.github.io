091c69bc1b4d92cf78bbdcb908d44819eb9010a7
mattwaelder <mattwaelder@gmail.com>
2022-05-18 01:09:55 -0700

    <added first itteration of touch navigation for gallery images>

--- script_gal.js
+++ script_gal.js
@@ -12,17 +12,20 @@ const selectedImgContainer = document.getElementById("selected_img_container");
 const selectedImg = document.getElementById("selected_img");
 
 //fn to populate the gallery
+const HARDCODEIMAGEVALUE = 46;
+let selectedIndex = null;
 const populateGal = function () {
-  let selectedIndex = null;
-
   ///////////////////////////////////GALLERY HARD CODE AMOUNT/////////////////////////////////////
   //make new array with length equal to amount of pics in gallery
-  const countArr = new Array(46);
+  const countArr = new Array(HARDCODEIMAGEVALUE);
 
   //create new images with data-src;
   for (let i = 1; i < countArr.length + 1; i++) {
     let curImg = document.createElement("img");
     curImg.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
+
+    //making an index to use for arrow navigation of images
+    curImg.dataset.index = i;
 
     //toggle for int. obs. lazy loading;
     // curImg.dataset.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
@@ -36,6 +39,8 @@ const populateGal = function () {
     curImg.addEventListener("click", () => {
       selectedImgContainer.style.transform = `translateY(0)`;
       selectedImg.src = `../media/gallery_imgs/gal_img (${i}).jpg`;
+      selectedImg.dataset.index = i;
+      selectedIndex = i;
     });
 
     galleryContainer.appendChild(curImg);
@@ -51,7 +56,69 @@ selectedImgContainer.addEventListener("click", function () {
 });
 
 //////////////////////////////////////////////////////////////////////////////
-/* Int Obs API lazy loading seems to suck with grid */
+/* gallery image navigation */
+//////////////////////////////////////////////////////////////////////////////
+const prevImg = function () {
+  //if on first image
+  if (selectedIndex === 1) {
+    selectedImg.src = `../media/gallery_imgs/gal_img (${HARDCODEIMAGEVALUE}).jpg`;
+    selectedIndex = HARDCODEIMAGEVALUE;
+    return;
+  }
+
+  selectedImg.src = `../media/gallery_imgs/gal_img (${selectedIndex - 1}).jpg`;
+  selectedIndex--;
+};
+
+const nextImg = function () {
+  //if on last image
+  if (selectedIndex === HARDCODEIMAGEVALUE) {
+    selectedImg.src = `../media/gallery_imgs/gal_img (${1}).jpg`;
+    selectedIndex = 1;
+    return;
+  }
+
+  selectedImg.src = `../media/gallery_imgs/gal_img (${selectedIndex + 1}).jpg`;
+  selectedIndex++;
+};
+
+document.addEventListener("keydown", function (e) {
+  e.preventDefault();
+  const key = e.key;
+  if (!key) return;
+  if (key === "ArrowLeft") {
+    prevImg();
+  } else if (key === "ArrowRight") {
+    nextImg();
+  }
+});
+
+//////////////////////////////////////////////////////////////////////////////
+/* mobile touch navigation */
+//////////////////////////////////////////////////////////////////////////////
+let startX;
+let endX;
+selectedImg.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  startX = e.touches[0].screenX;
+});
+
+//issuing callback functions at the end of the touch, its slow in devtools but works :o
+selectedImg.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  endX = e.changedTouches[0].screenX;
+
+  let swipeLen = Math.abs(endX - startX);
+  console.log(swipeLen);
+
+  if (swipeLen >= 50) {
+    if (startX < endX) prevImg();
+    if (startX > endX) nextImg();
+  }
+});
+
+//////////////////////////////////////////////////////////////////////////////
+/* Int Obs API lazy loading seems to suck with grid*/
 //////////////////////////////////////////////////////////////////////////////
 
 // const lazyImgs = document.querySelectorAll("[data-src]");
